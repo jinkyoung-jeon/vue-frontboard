929f8130150cea27403e4d438fea82b20613b62e
JeonJinKyoung <kyeong1750@naver.com>
2023-03-23 18:31:56 +0900

    https://onethejay.tistory.com/66 Vue.js 게시판 만들기 3 - 레이아웃 분할

---
+++ src/views/PageAbout.vue
@@ -0,0 +1,6 @@
+<!-- PageAbout.vue -->
+<template>
+    <div class="about">
+        <h1>여기는 PageAbout 페이지 입니다.</h1>
+    </div>
+</template>
---
+++ src/views/PageHome.vue
@@ -0,0 +1,18 @@
+<!-- PageHome.vue -->
+<template>
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+    name: 'PageHome',
+    components: {
+        HelloWorld
+    }
+}
+</script>
